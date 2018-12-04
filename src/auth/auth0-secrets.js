66dcbe53baa8c7eee28cc876f5fb400052aadaa9
Edward Lim <sgedwardlim@gmail.com>
2018-12-03 20:01:53 -0800

    Added SSL to site

--- src/auth/auth0-secrets.js
+++ src/auth/auth0-secrets.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'corate.auth0.com',
   clientId: 'pe6YJ3kVKYosQeXMY_688-BPj1Uwcw-k',
-  callbackUrl: 'http://35.229.81.183/callback'
+  callbackUrl: 'https://35.229.81.183/callback'
 }
